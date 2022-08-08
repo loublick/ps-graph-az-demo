00e61f1216bef82d6d6897658f94d0c7d8b2ddf2
loublick <loublick@microsoft.com>
2022-08-08 17:45:17 -0400

    Remove hardcoded SubscriptionId

--- src/ps/app-service-deploy.ps1
+++ src/ps/app-service-deploy.ps1
@@ -7,8 +7,8 @@ $location="East US"
 $resourceGroupName="rg-ps-graph-az-demo"
 
 # Set the Azure context
-Set-AzContext -Subscription "38f49f24-d0b7-461a-9801-85c65fc65628" -Force
-Set-AzContext -Tenant "8df4579d-a09d-4eae-828e-42f483ca5f70" -Force
+#Set-AzContext -Subscription "38f49f24-d0b7-461a-9801-85c65fc65628" -Force
+#Set-AzContext -Tenant "8df4579d-a09d-4eae-828e-42f483ca5f70" -Force
 
 # Create a resource group.
 New-AzResourceGroup -Name $resourceGroupName -Location $location
